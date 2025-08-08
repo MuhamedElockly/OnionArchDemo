8f5ecab4941586aec2351c09357498ce7b079c63
dell <elocklymuhamed@gmail.com>
2025-08-09 02:52:19 +0300

    Add project files.

---
+++ Presistence/PresistenceLayerConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Presistence.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presistence
+{
+	public static class PresistenceLayerConfiguration
+	{
+		public static IServiceCollection AddPresistenceConfig(this IServiceCollection services, IConfiguration configuration)
+		{
+			services.AddDbContext<ApplicationDbContext>(options =>
+			options.UseSqlServer(
+				configuration.GetConnectionString("DefaultConnection"),
+			sqlOptions => sqlOptions.EnableRetryOnFailure()
+			));
+			return services;
+		}
+	}
+}
